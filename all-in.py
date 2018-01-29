6bb4e0acec23683ae835e78b3b668da498b46c0e
se4 <se4@live.cn>
2018-01-29 16:36:23 +0800

    bug fixed

--- all-in.py
+++ all-in.py
@@ -6,8 +6,9 @@ import requests
 from plugins.huobi.HuobiServices import get_balance, send_order
 
 
-COIN1 = 'ruff'.lower()
+COIN1 = 'lun'.lower()
 COIN2 = 'eth'.lower()
+MIN_NUM = 780
 
 
 def get_tote(coin):
@@ -24,7 +25,7 @@ def get_tote(coin):
 def get_price(symbol):
     addr = 'https://api.huobi.pro/market/trade?symbol={symbol}'
     resp_json = requests.get(addr.format(symbol=symbol)).text
-    if 'null' not in resp_json:
+    if 'invalid' not in resp_json:
         resp_dict = json.loads(resp_json)
         sell_price = resp_dict['tick']['data'][0]['price']
         return '%f' % sell_price
@@ -41,22 +42,26 @@ def cal_num(coin1, coin2='eth'):
         coin1_price = float(get_price(coin1 + coin2))
         print('{} 的价格为 {}'.format(coin1, coin1_price))
     coin2_balance = float(get_tote(coin2))
-    print('{} 我拥有的总量为 {}'.format(coin1, coin2_balance))
+    print('{} 我拥有的总量为 {}'.format(coin2, coin2_balance))
     max_number = coin2_balance/coin1_price
     return '%.4f' % max_number
 
 
 def all_in(coin1, coin2):
-    amount = float(cal_num(coin1, coin2)) - 20
-    resp_dict = send_order(amount=str(amount), source='', symbol=coin1 + coin2, _type='buy-market')
-    print('火币接口返回字典为：')
-    print(resp_dict)
-    return resp_dict
+    amount = float(cal_num(coin1, coin2))
+    print('可供购买：{} 个'.format(amount))
+    if amount > MIN_NUM:
+        resp_dict = send_order(amount=str(get_tote(coin2)), source='', symbol=coin1 + coin2, _type='buy-market')
+        print('执行购买：')
+        print(resp_dict)
+        return resp_dict
+    else:
+        return {'status': 'ex'}
 
 
 def task():
     while all_in(COIN1, COIN2)['status'] != 'ok':
-        print('\033[34;1m [主程序] 正在尝试用 {} 购买 {} ...\033[0m'.format(COIN2, COIN1))
+        print('\033[34;1m [当前任务] 探测 {} 购买 {}  ...\033[0m'.format(COIN2, COIN1))
     print('\033[36;1m 已经买到，程序退出... \033[0m')
     return
 
